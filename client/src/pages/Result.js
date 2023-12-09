78c9267942cc1f7786b283d95975684c880fce3e
Mohammad Aasif <kasifsaif784@gmail.com>
2023-12-09 17:55:56 +0530

    result page responsive

--- client/src/pages/Result.js
+++ client/src/pages/Result.js
@@ -30,75 +30,71 @@ const Result = () => {
   } = videoDetails;
 
   return (
-    <div>
-      <div className="w-full py-10 h-[359px]  bg-stone-950 justify-center items-center inline-flex">
-        <div className="self-stretch px-10 py-5 bg-stone-900 rounded-2xl justify-start items-center gap-5 inline-flex">
-          <div className="justify-start items-center gap-10 flex">
-            <div className="justify-start items-center gap-5 flex">
-              <div className="w-[254px] flex-col justify-start items-start gap-5 inline-flex">
-                <div className="px-2 py-1 bg-neutral-500 rounded justify-start items-center gap-1 inline-flex">
-                  <div className="w-6 h-6 relative" />
-                  <div className="text-white text-base font-normal font-['Inter'] leading-tight">
-                    Top earner video
-                  </div>
-                </div>
-                <img
-                  className="w-60 h-[135px] relative rounded-[10px]"
-                  src={thumbnail || "https://via.placeholder.com/240x135"}
-                />
+    <div className="sm:my-10 xs:my-10">
+      <div className="w-full md:py-20 h-[359px]  bg-stone-950 justify-center items-center inline-flex">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-10 py-5 bg-stone-900 rounded-2xl">
+          <div className="w-full flex-col justify-center items-start gap-5">
+            <div className="px-2 py-1 bg-neutral-500 rounded justify-start items-center gap-1 inline-flex">
+              <div className="w-6 h-6 relative" />
+              <div className="text-white text-base font-normal font-['Inter'] leading-tight">
+                Top earner video
+              </div>
+            </div>
+            <img
+              className="w-60 h-[135px] relative rounded-[10px]"
+              src={thumbnail || "https://via.placeholder.com/240x135"}
+            />
+            <div className="text-white text-opacity-50 text-base font-normal font-['Inter']">
+              Uploaded on - {uploadedOn}
+            </div>
+          </div>
+
+          <div className="w-full flex-col justify-start items-start gap-[11px] inline-flex">
+            <div className="h-[50px] text-white text-md font-medium font-['Inter']">
+              {title.substring(0, 40)}
+            </div>
+            <div className="flex-col justify-start items-start gap-3 flex">
+              <div className="justify-start items-center gap-2 inline-flex">
+                <FaEye className="text-slate-100" />
                 <div className="text-white text-opacity-50 text-base font-normal font-['Inter']">
-                  Uploaded on - {uploadedOn}
+                  {views}
                 </div>
               </div>
-              <div className="w-[255px] flex-col justify-start items-start gap-[11px] inline-flex">
-                <div className="self-stretch h-[50px] text-white text-md font-medium font-['Inter']">
-                  {title.substring(0,40)}
+              <div className="justify-start items-center gap-2 inline-flex">
+                <FaRegThumbsUp className="text-slate-100" />
+                <div className="text-white text-opacity-50 text-base font-normal font-['Inter']">
+                  {likes}
                 </div>
-                <div className="flex-col justify-start items-start gap-3 flex">
-                  <div className="justify-start items-center gap-2 inline-flex">
-                    {/* <div className="w-6 h-6 relative" /> */}
-                    <FaEye className="text-slate-100" />
-                    <div className="text-white text-opacity-50 text-base font-normal font-['Inter']">
-                      {views}
-                    </div>
-                  </div>
-                  <div className="justify-start items-center gap-2 inline-flex">
-                    {/* <div className="w-6 h-6 relative" /> */}
-                    <FaRegThumbsUp className="text-slate-100" />
-                    <div className="text-white text-opacity-50 text-base font-normal font-['Inter']">
-                      {likes}
-                    </div>
-                  </div>
-                  <div className="justify-start items-center gap-2 inline-flex">
-                    <FaMessage className="text-slate-100" />
-                    <div className="text-white text-opacity-50 text-base font-normal font-['Inter']">
-                      {comments}
-                    </div>
-                  </div>
+              </div>
+              <div className="justify-start items-center gap-2 inline-flex">
+                <FaMessage className="text-slate-100" />
+                <div className="text-white text-opacity-50 text-base font-normal font-['Inter']">
+                  {comments}
                 </div>
               </div>
             </div>
-            <div className="px-5 py-10 bg-zinc-800 rounded-2xl flex-col justify-start items-start gap-2.5 inline-flex">
-              <div className="h-[120px] flex-col justify-start items-center gap-6 flex">
-                <div className="justify-start items-center inline-flex">
-                  <div className="w-10 h-10 pl-[9.50px] pr-[9.41px] py-1 justify-center items-center flex">
-                    <div className="w-[21.09px] h-8 relative flex-col justify-start items-start flex" />
-                  </div>
-                  <div className="text-white text-[40px] font-bold font-['Inter']">
-                    $ {earnings}
-                  </div>
+          </div>
+
+          <div className="px-5 py-10 bg-zinc-800 rounded-2xl flex-col justify-start items-start gap-2.5 inline-flex">
+            <div className="h-[120px] flex-col justify-start items-center gap-6 flex">
+              <div className="justify-start items-center inline-flex">
+                <div className="w-10 h-10 pl-[9.50px] pr-[9.41px] py-1 justify-center items-center flex">
+                  <div className="w-[21.09px] h-8 flex-col justify-start items-start flex" />
                 </div>
-                <div className="h-12 px-6 py-[18px] bg-white rounded-[35px] justify-center items-center gap-2.5 inline-flex">
-                  <div className="text-neutral-900 text-base font-normal font-['Inter']">
-                    Check How?
-                  </div>
+                <div className="text-white text-[40px] font-bold font-['Inter']">
+                  $ {earnings}
+                </div>
+              </div>
+              <div className="h-12 px-6 py-[18px] bg-white rounded-[35px] justify-center items-center gap-2.5 inline-flex">
+                <div className="text-neutral-900 self-center font-normal font-['Inter']">
+                  Check How?
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="text-white text-center text-opacity-70 text-xl font-medium font-['Inter'] leading-7">
+      <div className="text-white sm:mt-20 xs:mt-36 text-center text-opacity-70 text-xl font-medium font-['Inter'] leading-7">
         Other Videos Potentials
       </div>
       <div
@@ -142,7 +138,7 @@ const Result = () => {
               >
                 89
               </th>
-              <td className="px-6 py-4">{title.substring(0,5)}</td>
+              <td className="px-6 py-4">{title.substring(0, 5)}</td>
               <td className="px-6 py-4">
                 <img
                   className="w-20 h-20"
